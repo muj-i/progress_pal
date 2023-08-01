5c01f984aaf602c240bc58e0964a8d3758a098d2
MUJ <99077742+muj-i@users.noreply.github.com>
2023-08-01 09:53:41 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:progress_pal/app.dart';
 
 // todo class 2 mod 12 - 35 min recap
-//  todo: mod 12 class 2 -> 01.02 minutes
+//  todo: mod 12 class 2 -> 01.06 minutes.
 // todo: fix the user infonot loaded at the begining
 
 void main() {
