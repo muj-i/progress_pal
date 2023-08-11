dfb37f5a36a25eb534dd1092ab4c5f0658004aad
MUJ <99077742+muj-i@users.noreply.github.com>
2023-08-11 21:00:41 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,40 @@ import 'package:progress_pal/app.dart';
 void main() {
   runApp(const ProgressPalApp());
 }
+
+
+//! const - email_verify_page
+//! const - login_page
+//! const - pin_verify_page
+//! const - reset_password_page
+//! const - signup_page
+
+//! const - update_profile_page
+//! const - update_password_page
+//! const - update_task_page
+//! const - update_task_status
+
+//! const - about_page
+//! const - add"_new_task
+//! const - bottomnav_page
+//! const - cancelled_task
+//! const - completed_task
+//! const - inprogress_task
+//! const - new_task
+//! const - splash_page
+
+//! const - asset utils
+
+//! const - constraints
+//! const - dialog
+//! const - profile
+//! const - screen background
+//! const - task list tile
+//! const - task summary
+
+//! const - APP
+//! const - MAIN
+
+//* GetX implementation
+
+
