8e5dd500a0c0403efa1350a71acdc5af7c4d2d13
MUJ <99077742+muj-i@users.noreply.github.com>
2023-08-13 05:05:15 +0600

    removed everything

--- android/app/src/main/kotlin/com/muj/progresspal/progress_pal/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.muj.progresspal.progress_pal
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/ui/utils/assets_utils.dart
+++
@@ -1,6 +0,0 @@
-class AssetUtils {
-  static const String _imagePath = 'assets/images';
-
-  static const String logoSVG = '$_imagePath/logo3.svg';
-  static const String backgroundSVG = '$_imagePath/rainbow-vortex1.svg';
-}
